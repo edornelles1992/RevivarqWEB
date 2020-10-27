57738d72de94e317f39c31da5d177ef5fa905b49
Maurício Lima <mauricio.lima.001@acad.pucrs.br>
2020-10-26 21:06:23 -0300

    fix - campos editáveis.

--- src/components/AreaCliente/index.js
+++ src/components/AreaCliente/index.js
@@ -108,11 +108,24 @@ export default class SwitchListSecondary extends React.Component {
 		}
 		else {
 			showNotification(updateUser.data.message)
+			//FIX - Campos editaveis
+			var estado = this.state.trocaBotao
+			if (estado) {
+				this.setState({
+					trocaBotao: false,
+					abreEdicao: false,
+					disabled: false
+				})
+			}
+			else {
+				this.setState({
+					trocaBotao: true,
+					abreEdicao: true,
+					disabled: true
+				})
+			}
 			return true
-		}
-
-
-
+		}		
 	}
 
 	renderContent(reform, index) {
@@ -132,7 +145,7 @@ export default class SwitchListSecondary extends React.Component {
 				<TableCell align="right" style={{borderBottomRightRadius:"15px"}}><EditIcon style={{ cursor: 'pointer', color: 'rgb(21,38,32)' }} onClick={(e) => this.handleClick(reform)} /></TableCell>
 			</TableRow>
 		)
-	}
+	}	
 
 
 	handleClickvoltar() {
